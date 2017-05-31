328f635cde1d3036ecdd0df0d3b0ed5f3ad049f2
Reza Rg <re.ranjbaran@gmail.com>
2017-05-31 15:47:12 +0430

    Updated for swift 3.1

--- Base64.swift
+++ Base64.swift
@@ -1,1 +1,26 @@
+//
+//  Base64.swift
+//  twitt3r
+//
+//  Created by Reza Rg on 3/1/15.
+//  Copyright (c) 2015 ghaleh. All rights reserved.
+//
 
+import Foundation
+
+extension String {
+    
+    //Encode base64
+    func base64Encoded() -> String {
+        let plainData = self.data(using: String.Encoding.utf8)
+        let base64String = plainData?.base64EncodedString(options: [])
+        return base64String!
+    }
+    
+    //Decode base64
+    func base64Decoded() -> String {
+        let decodedData = Data(base64Encoded: self, options: [])
+        let decodedString = NSString(data: decodedData!, encoding: String.Encoding.utf8.rawValue)
+        return decodedString! as String
+    }
+}
